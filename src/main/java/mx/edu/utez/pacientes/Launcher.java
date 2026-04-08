4abf8797e1a2e43e6d31934875dc3bdd6b5fdf71
Andrecitoflow <20253ds094@utez.edu.mx>
2026-04-08 08:29:20 -0600

    Resureccion de mi rama

--- src/main/java/mx/edu/utez/pacientes/Launcher.java
+++ src/main/java/mx/edu/utez/pacientes/Launcher.java
@@ -2,7 +2,15 @@ package mx.edu.utez.pacientes;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class
+
+
+  dd
+
+
+
+
+        gLauncher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }

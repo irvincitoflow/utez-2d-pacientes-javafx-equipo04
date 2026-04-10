88549c7aa257f350b9d442db4e9e0bd0d47b0a56
Andrecitoflow <20253ds094@utez.edu.mx>
2026-04-10 14:49:14 -0600

    EN LAUNCHER te deje un coment peque

--- src/main/java/mx/edu/utez/pacientes/Launcher.java
+++ src/main/java/mx/edu/utez/pacientes/Launcher.java
@@ -7,3 +7,5 @@ public class Launcher {
         Application.launch(HelloApplication.class, args);
     }
 }
+//Pequeño voy a llevar a mi abuelito a unos estudios ya cheque las correciones que vimos ayer creo que va joya
+//ya corre aqui en dev puedes checar las buenas practica y hacer colaborador al Profe ya no me da tiempo a mi porfa pero te dejp listo el dev para que hagas el git pull desde tu lap :v
